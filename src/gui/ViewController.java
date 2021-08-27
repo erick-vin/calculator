36e59dd5e5904701ab6b15001e281de752e3df24
Erick Vinicius <ericktjdev@gmail.com>
2021-08-27 12:44:09 -0300

    correct import's

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -3,7 +3,6 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import application.Main;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
